cc644fbcbf9268e9a6d40b773f569e34a415557a
hasanaltin <62545852+hasanaltin@users.noreply.github.com>
2022-09-13 17:08:31 +0200

    Create DateandTime.ps1

---
+++ CommandPrompt/DateandTime.ps1
@@ -0,0 +1,16 @@
+## Timezone for Computer:
+
+Set-TimeZone -name "Turkey Standard Time"
+
+## Starts w32time service and register time service to run as a service:
+
+Start-Service w32time
+w32tm /register
+
+## Add all ntp.pool.org ntp servers:
+
+w32tm /config /syncfromflags:manual /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org"
+
+## Force resync:
+
+w32tm /resync /force
